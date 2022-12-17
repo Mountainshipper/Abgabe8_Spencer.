ebfec621c3de4a9067182bf46e5466c788138949
samuel <samuel.spencer@edu.fh-joanneum.at>
2022-12-17 21:57:34 +0100

    Finish

--- app/src/main/java/com/example/joanneumprojekt/open/Change_DATA_1_User.java
+++ app/src/main/java/com/example/joanneumprojekt/open/Change_DATA_1_User.java
@@ -24,8 +24,8 @@ import com.shashank.sony.fancytoastlib.FancyToast;
 import java.util.List;
 
 public class Change_DATA_1_User extends AppCompatActivity implements View.OnClickListener {
-    private Button btn_GET_ALL_INFO, btn_Upload_Info_ALL;
-    private TextView txt_Get_ALL_INFO, txt_Admin_Choose_Username, txt_Admin_Choose_ID, txt_Admin_Choose_Password, txt_Admin_Choose_Email, txt_Admin_Choose_User, txt_Admin_Choose_Slots;
+    private Button btn_GET_ALL_Work, btn_Upload_Work_ALL;
+    private TextView txt_Get_ALL_WORK, txt_Admin_Choose_Work, txt_Admin_Choose_USER, txt_Admin_Choose_Title, txt_Admin_Choose_Function;
     String txt_All = "";
 
 
@@ -35,36 +35,33 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_data1_user);
 
-
-        btn_GET_ALL_INFO = findViewById(R.id.btn_GET_ALL_INFO);
-        btn_Upload_Info_ALL = findViewById(R.id.btn_Upload_Info_ALL);
-        txt_Get_ALL_INFO = findViewById(R.id.txt_Get_ALL_INFO);
-        txt_Admin_Choose_Username = findViewById(R.id.txt_Admin_Choose_Username);
-        txt_Admin_Choose_ID = findViewById(R.id.txt_Admin_Choose_ID);
-        txt_Admin_Choose_Password = findViewById(R.id.txt_Admin_Choose_Password);
-        txt_Admin_Choose_Email = findViewById(R.id.txt_Admin_Choose_Email);
-        txt_Admin_Choose_User = findViewById(R.id.txt_Admin_Choose_User);
-        txt_Admin_Choose_Slots = findViewById(R.id.txt_Admin_Choose_Slots);
+        btn_GET_ALL_Work = findViewById(R.id.btn_GET_ALL_Work);
+        btn_Upload_Work_ALL = findViewById(R.id.btn_Upload_Work_ALL);
+        txt_Get_ALL_WORK = findViewById(R.id.txt_Get_ALL_WORK);
+        txt_Admin_Choose_Work = findViewById(R.id.txt_Admin_Choose_Work);
+        txt_Admin_Choose_USER = findViewById(R.id.txt_Admin_Choose_USER);
+        txt_Admin_Choose_Title = findViewById(R.id.txt_Admin_Choose_Title);
+        txt_Admin_Choose_Function = findViewById(R.id.txt_Admin_Choose_Function);
 
 
-        btn_GET_ALL_INFO.setOnClickListener(this);
-        btn_Upload_Info_ALL.setOnClickListener(this);
+        btn_GET_ALL_Work.setOnClickListener(this);
+        btn_Upload_Work_ALL.setOnClickListener(this);
 
 
     }
 
-    int n = 0;
+
 
     @Override
     public void onClick(View view) {
 
         switch (view.getId()) {
-            case R.id.btn_GET_ALL_INFO:
+            case R.id.btn_GET_ALL_Work:
                 get_All_Data();
                 break;
 
 
-            case R.id.btn_Upload_Info_ALL:
+            case R.id.btn_Upload_Work_ALL:
                 setUser();
                 break;
 
@@ -80,9 +77,9 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
     public void get_All_Data() {
 
         txt_All = "";
-        txt_Get_ALL_INFO.setText("");
+        txt_Get_ALL_WORK.setText("");
 
-        ParseQuery<ParseObject> queryAllProfessor = ParseQuery.getQuery("New_User");
+        ParseQuery<ParseObject> queryAllProfessor = ParseQuery.getQuery("All_Works");
         queryAllProfessor.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
@@ -90,9 +87,9 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
                 if (e == null) {
                     if (objects.size() > 0) {
                         for (ParseObject parseObject : objects) {
-                            txt_All = txt_All + "--------------\nUsername: " + parseObject.get("Username") + ". \nID: " + parseObject.get("ID") + ". \nPassword:  " + parseObject.get("password") +
-                                    ". \nEmail: " + parseObject.get("email")+ ". \nSlots: " + parseObject.get("Slots")  + "\n\n\n";
-                            txt_Get_ALL_INFO.setText(txt_All);
+                            txt_All = txt_All + "--------------\nTitle: " + parseObject.get("Title") + ". \nFunction: " + parseObject.get("Function") + ". \nAvailability:  " + parseObject.get("User") +
+                                    ". \nDeadline: " + parseObject.get("Deadline") + ". \nExam Date: " + parseObject.get("Exam_Date") +  "\n\n\n";
+                            txt_Get_ALL_WORK.setText(txt_All);
                             ++count_Assistent;
                         }
                     }
@@ -103,26 +100,12 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
 
 
     public void setUser() {
-        if (txt_Admin_Choose_User.getText().toString().isEmpty()) {
-            FancyToast.makeText(Change_DATA_1_User.this, "Please select a 'USER'", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+        if (txt_Admin_Choose_Work.getText().toString().isEmpty()) {
+            FancyToast.makeText(Change_DATA_1_User.this, "Please select a 'WORK'", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
 
         } else {
-            if (txt_Admin_Choose_Password.getText().toString().isEmpty() && txt_Admin_Choose_Email.getText().toString().isEmpty()) {
-                set();
-            }else if (txt_Admin_Choose_Password.getText().toString().length() > 4) {
-                    if ((txt_Admin_Choose_Email.getText().toString().contains("@") || txt_Admin_Choose_Email.getText().toString().contains(".at") || txt_Admin_Choose_Email.getText().toString().contains(".com"))
-                            && txt_Admin_Choose_Email.getText().toString().length() > 6) {
-                        set();
 
-                    } else {
-                        FancyToast.makeText(Change_DATA_1_User.this, "Please wite a valid Email", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
-
-                    }
-                } else {
-                    FancyToast.makeText(Change_DATA_1_User.this, "Password to short", FancyToast.LENGTH_SHORT, FancyToast.ERROR, true).show();
-                }
-
-
+            set();
         }
     }
 
@@ -134,8 +117,8 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
 
         Current_Login current_user = (Current_Login) getIntent().getSerializableExtra("current_user");
 
-        ParseQuery<ParseObject> query2 = ParseQuery.getQuery("New_User");
-        query2.whereEqualTo("Username", txt_Admin_Choose_User.getText().toString());
+        ParseQuery<ParseObject> query2 = ParseQuery.getQuery("All_Works");
+        query2.whereEqualTo("Title", txt_Admin_Choose_Work.getText().toString());
 
 
         query2.getFirstInBackground(new GetCallback<ParseObject>() {
@@ -143,30 +126,22 @@ public class Change_DATA_1_User extends AppCompatActivity implements View.OnClic
                 if (e == null) {
                     progressDialog.dismiss();
 
-                    if (txt_Admin_Choose_Username.getText().toString().isEmpty()) {
+
+                    if (txt_Admin_Choose_USER.getText().toString().isEmpty()) {
 
                     } else {
-                        object.put("Username", txt_Admin_Choose_Username.getText().toString());
+                        object.put("User", txt_Admin_Choose_USER.getText().toString());
                     }
-                    if (txt_Admin_Choose_ID.getText().toString().isEmpty()) {
+
+                    if (txt_Admin_Choose_Title.getText().toString().isEmpty()) {
 
                     } else {
-                        object.put("ID", txt_Admin_Choose_ID.getText().toString());
+                        object.put("Title", txt_Admin_Choose_Title.getText().toString());
                     }
-                    if (txt_Admin_Choose_Slots.getText().toString().isEmpty()) {
+                    if (txt_Admin_Choose_Function.getText().toString().isEmpty()) {
 
                     } else {
-                        object.put("Slots", txt_Admin_Choose_Slots.getText().toString());
-                    }
-                    if (txt_Admin_Choose_Password.getText().toString().isEmpty()) {
-
-                    } else {
-                        object.put("password", txt_Admin_Choose_Password.getText().toString());
-                    }
-                    if (txt_Admin_Choose_Email.getText().toString().isEmpty()) {
-
-                    } else {
-                        object.put("email", txt_Admin_Choose_Email.getText().toString());
+                        object.put("Function", txt_Admin_Choose_Function.getText().toString());
                     }
 
 
