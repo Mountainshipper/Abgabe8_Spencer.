4d7b174e8e885f484af523ecb6c9f293c32526ae
samuel <samuel.spencer@edu.fh-joanneum.at>
2022-12-17 18:22:29 +0100

    add delete Bill

--- app/src/main/java/com/example/joanneumprojekt/Delete_Bill/activity_admin_delete_user.java
+++ app/src/main/java/com/example/joanneumprojekt/Delete_Bill/activity_admin_delete_user.java
@@ -7,12 +7,20 @@
 package com.example.joanneumprojekt.Delete_Bill;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.joanneumprojekt.After_Login.New_Bill;
+import com.example.joanneumprojekt.After_Login.show_delete_bill;
 import com.example.joanneumprojekt.R;
 import com.parse.DeleteCallback;
 import com.parse.FindCallback;
@@ -25,6 +33,7 @@ import java.util.List;
 
 public class activity_admin_delete_user extends AppCompatActivity implements View.OnClickListener {
 
+    DrawerLayout drawerLayout;
     private Button Private_Button, Business_Button, Delete_Business, deletePrivate;
     private TextView txt_Display_Work, txt_Display_User, txt_Write_Work, txt_Write_User;
     String txt_Private, txtBusiness;
@@ -145,8 +154,6 @@ public class activity_admin_delete_user extends AppCompatActivity implements Vie
 
                                 if (work.size() > 0) {
 
-                                    redundant_Work();
-
                                     work.get(0).deleteInBackground(new DeleteCallback() {
                                         @Override
                                         public void done(ParseException e) {
@@ -191,8 +198,6 @@ public class activity_admin_delete_user extends AppCompatActivity implements Vie
                             if (e == null) {
 
                                     if (work.size() > 0) {
-
-                                        redundant_Work();
 
                                         work.get(0).deleteInBackground(new DeleteCallback() {
                                             @Override
@@ -254,7 +259,6 @@ public class activity_admin_delete_user extends AppCompatActivity implements Vie
                             object.put("User", "open");
                         }
                         object.saveInBackground();
-                        redundant_Work();
                     }
                 }
             }
@@ -262,56 +266,89 @@ public class activity_admin_delete_user extends AppCompatActivity implements Vie
     }
 
 
-    public void redundant_Work() {
 
-        ParseQuery<ParseObject> User = ParseQuery.getQuery("New_User");
-        User.findInBackground(new FindCallback<ParseObject>() {
+
+
+
+    public void clickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+    // von der Seite soll der navigator kommen
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+
+    //What should happen when you click on the logo in the drawer
+    public void clickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //When drawer is open, then close it
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    // wenn ich home bin, und dann im navigator auf home erneut klicke - passiert halt ein refresh
+    public void clickHome(View view){
+        //Recreate the Rob_MainActivity
+        recreate();
+    }
+
+    public void clickApplications(View view){
+        //Redirect current activity to Applications activity
+        // --> wenn ich auf add bill click, soll er zu add bill kommen
+        redirectActivity(this, New_Bill.class);
+    }
+
+
+    public void clickLogout(View view){
+        //close app
+        logout(this);
+    }
+
+    public void deleteUser(View view){
+        //close app
+        redirectActivity(this, show_delete_bill.class);
+    }
+
+    public static void logout(Activity activity) {
+        //Initialize the alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.logout_alert_title);
+        builder.setMessage(R.string.logout_alert_message);
+
+        //Positive button clicked
+        builder.setPositiveButton(R.string.logout_alert_responsePositive, new DialogInterface.OnClickListener() {
             @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-
-                if (e == null) {
-//                    FancyToast.makeText(activity_admin_delete_user.this, "" +txt_Write_Work.getText().toString(), FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
-
-                    for (ParseObject object : objects) {
-                        if (object.getString("Project_txt").isEmpty()) {
-
-                        } else {
-                            if (object.getString("Project_txt").equals(txt_Write_Work.getText().toString())) {
-                                object.put("Project_txt", "");
-                            }
-                        }
-                        if (object.getString("Bachelor_txt").isEmpty()) {
-
-                        } else {
-                            if (object.getString("Bachelor_txt").equals(txt_Write_Work.getText().toString())) {
-                                object.put("Bachelor_txt", "");
-                            }
-                        }
-                        if (object.getString("Master_txt").isEmpty()) {
-
-                        } else {
-                            if (object.getString("Master_txt").equals(txt_Write_Work.getText().toString())) {
-                                object.put("Master_txt", "");
-                            }
-                        }
-
-
-                        if (email.isEmpty()) {
-
-                        } else {
-                            if (object.getString("Work").contains(email)) {
-                                String hh = object.getString("Work");
-                                String strNew = hh.replaceFirst(email, "");
-
-                                object.put("Work", strNew);
-
-                            }
-                        }
-                        object.saveInBackground();
-                    }
-                }
+            public void onClick(DialogInterface dialogInterface, int i) {
+                activity.finishAffinity();
+                System.exit(0);
             }
         });
+
+        //Negative button clicked
+        builder.setNegativeButton(R.string.logout_alert_responseNegative, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+
+    public static void redirectActivity(Activity sourceActivity, Class classOfDestinationActivity) {
+        Intent intent = new Intent(sourceActivity, classOfDestinationActivity);
+        sourceActivity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 }
 
