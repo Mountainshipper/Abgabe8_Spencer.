e7f6de2bb991d59f2329b67bc6c268c8263aaa9e
samuel <samuel.spencer@edu.fh-joanneum.at>
2022-12-17 15:16:34 +0100

    test

--- app/src/main/java/com/example/joanneumprojekt/Display_Interface/show_add_bill.java
+++ app/src/main/java/com/example/joanneumprojekt/Display_Interface/show_add_bill.java
@@ -1,28 +1,10 @@
 package com.example.joanneumprojekt.Display_Interface;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.TextView;
 
 import com.example.joanneumprojekt.R;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.SaveCallback;
-import com.shashank.sony.fancytoastlib.FancyToast;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class show_add_bill extends AppCompatActivity {
     //Date
@@ -31,7 +13,7 @@ public class show_add_bill extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_add_bill);
+        setContentView(R.layout.show_add_bill);
 
 
     }
